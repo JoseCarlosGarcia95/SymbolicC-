0b532c5a5bcd1a85fc9b0837ac8cc01edf518a9d
JoseCarlosGarcia95 <hola@josecarlos.me>
2020-02-29 20:56:57 +0100

    Add metrics for total test execution time.

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+TOTAL_MS=0
 for f in test/*; do
   echo "Running tests for $f"
   g++ "$f" -l gtest -o ./run-test.out -lm -I headers/  -pthread || exit 1
-  ./run-test.out || exit 1
+  TEST_RESULT=$(./run-test.out || exit 1)
+  echo "$TEST_RESULT"
+  MS=$(echo "$TEST_RESULT" | grep "case ran." | awk '{print $9}')
+  MS="${MS:1}"
+  TOTAL_MS=$((TOTAL_MS + MS))
 done
+
+echo "TOTAL_MS=$TOTAL_MS ms"
