67e92d94e1e760a79fd3961375653d6bbae93478
JoseCarlosGarcia95 <hola@josecarlos.me>
2020-03-01 17:35:45 +0100

    Check memory with valgrind.

--- run-tests.sh
+++ run-tests.sh
@@ -5,10 +5,11 @@ for f in test/*; do
   echo "Running tests for $f"
   g++ "$f" -l gtest -o ./run-test.out -lm -I headers/  -pthread -lgmpxx -lgmp || exit 1
   TEST_RESULT=$(./run-test.out || exit 1)
-  echo "$TEST_RESULT"
   MS=$(echo "$TEST_RESULT" | grep "case ran." | awk '{print $9}')
   MS="${MS:1}"
   TOTAL_MS=$((TOTAL_MS + MS))
+
+  valgrind --error-exitcode=1 --leak-check=full ./run-test.out || exit 1
 done
 
 echo "TOTAL_MS=$TOTAL_MS ms"
